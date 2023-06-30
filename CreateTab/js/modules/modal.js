9bcce59c5b7b44fdcb3a148c5c80edc2ff5688b6
SokataGintoki <catarsislogos@gmail.com>
2023-06-30 15:55:55 +0300

    webpack.Разбираем наш проект, на отдельные части.

---
+++ CreateTab/js/modules/modal.js
@@ -0,0 +1,60 @@
+function modal() {
+        //Modal
+
+        const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal');
+
+    function openModal() {
+      modal.classList.add('show');
+      modal.classList.remove('hide');
+      document.body.style.overflow = 'hidden';
+      clearInterval(modalTimerId);
+        }
+
+modalTrigger.forEach(btn => {
+   btn.addEventListener('click', openModal);
+    // modal.classList.toggle('show');
+  });
+
+ 
+
+function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  };
+
+// modalCloseBtn.addEventListener('click', closeModal);
+  // modalCloseBtn.addEventListener('click', () => {
+  //   modal.classList.add('hide');
+  //   modal.classList.remove('show');
+  //   // modal.classList.toggle('show');
+  //   document.body.style.overflow = '';
+  // });
+
+modal.addEventListener('click', (e) => {
+    if ( e.target === modal || e.target.getAttribute('data-close') =='') {
+      closeModal();
+    }
+  });
+
+document.addEventListener('keydown', (e) => {
+      if(e.code === 'Escape' && modal.classList.contains('show')) {
+        closeModal();
+      }
+  });
+
+const modalTimerId = setTimeout(openModal, 50000);
+
+function showModalByScroll() {
+  if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1){
+    openModal();
+    window.removeEventListener('scroll', showModalByScroll);
+  }
+}
+window.addEventListener('scroll', showModalByScroll);
+
+
+}
+
+module.exports = modal;
